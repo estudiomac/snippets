602989a6597ce2fe7157adf5a1f8b537b4417d1a
Evgeniy <nsproster@gmail.com>
2018-04-06 08:29:57 +1000

    Create scroll-inside.js

---
+++ scroll-inside.js
@@ -0,0 +1,21 @@
+if (listContainer.length > 20) {
+	$("#tab-tab3-name ul").css({
+		height: '400px',
+		overflowY: "scroll"
+	}).append('<button class="btn btn-default" id="scrolldown" style="position: absolute; top: 50%; left: 80%;"><i class="fa fa-chevron-down" aria-hidden="true" style="font-size: 16px;"></i></button><button class="btn btn-default" id="scrollup" style="position: absolute; top: 40%; left: 80%;"><i class="fa fa-chevron-up" aria-hidden="true" style="font-size: 16px;"></i></button>')
+	.attr('id', 'listscrollable');
+	$(listContainer).last().attr('id', 'endlist');
+}
+
+$("#scrolldown").click(function(e) {
+  $('#listscrollable').animate({
+      scrollTop: $(e.currentTarget).position().top + $('#listscrollable').scrollTop()
+    },
+    'slow');
+});
+$("#scrollup").click(function(e) {
+  $('#listscrollable').animate({
+      scrollTop: $(e.currentTarget).position().top - $('#listscrollable').scrollTop()
+    },
+    'slow');
+});
