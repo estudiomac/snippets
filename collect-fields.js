c33b0c005bb6d5ddc992a71eb781beef8c97945f
Evgeniy <nsproster@gmail.com>
2018-08-21 01:24:29 +1000

    Create collect-fields.js

---
+++ collect-fields.js
@@ -0,0 +1,14 @@
+function collectFields() {	
+	
+	$("#uoq, #uol, #umq, #uml, #rutschhemmend").bind('keypress blur', function(){
+		var checkBox = $("#rutschhemmend");
+		($(checkBox).is(":checked")) ? $(checkBox).val("rutschhemmend") : $(checkBox).val("");
+ 
+		$("input.bumper").val('UOQ = ' + $("#bumperSize input#uoq").val() + " UOL = " +
+							  $("#bumperSize input#uol").val() + " UMQ = " +
+							  $("#bumperSize input#umq").val() + " UML = " +
+							  $("#bumperSize input#uml").val() + " | " +
+							  $(checkBox).val()
+							  );
+	});	
+}
